ee15d85d821a4b8df1686b0bbae4680c26feb5c1
ramya749 <ramyapharathi@gmail.com>
2025-03-03 22:57:08 -0400

    get data from axios

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button,Form } from 'react-bootstrap';
+import { Button,Container,Form } from 'react-bootstrap';
 import{useState} from "react";
 import{useNavigate} from "react-router-dom";
 
 import {setRegisterUsers} from '../redux/slices/register.js';
 import { useSelector,useDispatch } from 'react-redux';
+import image from '../assets/compter.jpg';
+import axios from 'axios';
 
 
 function Register(){
@@ -43,30 +45,79 @@ function Register(){
 
            alert("ok")
         setUser({name:"",email:"",password:""})
-        navigate('/login')
+        //  navigate('/login')
         }
     }
     else{
         alert("please fillup")
     }
+    const formData = new FormData();
+    formData.append("name",user.name);
+    formData.append("email",user.email);
+    formData.append("password",user.password);
+        axios.post("https://agaram.academy/api/b4/action.php?request=ai_carrier_user_register",formData)
+        .then((res)=>{
+            console.log(res)
+        })
 }
 
-    return <div>
+
+
+
+    return <div style={
+        {
+            backgroundImage: `url(${image})`,
+            backgroundSize: "cover",
+            backgroundAttachment: "revert",
+            backgroundRepeat: "no-repeat",
+            height: "620px",
+        }
+    }>
 
        {JSON.stringify(registerGlobalState)}
+    <Container style={
+        {
+            width :  "500PX",
+        }
+    }>
+                    <h1 style={{
+                        textAlign:"center"
+                    }}>Register</h1>
 
-        <h1>Register</h1>
-        Name
-        <Form.Control type="text" placeholder="enter your name" onChange={(e)=>setUser({...user,name:e.target.value})} ></Form.Control> 
-        Email
-        <Form.Control type="text" placeholder="enter your email" onChange={(e)=>setUser({...user,email:e.target.value})} ></Form.Control> 
-        Password
-        <Form.Control type="text" placeholder="enter your password" onChange={(e)=>setUser({...user,password:e.target.value})} ></Form.Control> 
+        <Form style={
+            
+                {
+                    backgroundColor: "rgba(8, 247, 207, 0.38)",
+                    WebkitBackdropFilter: "blur(5px)",
+                    backdropFilter: "blur(5px)",
+                    marginTop: "20px",
+                    padding: "30px",
+                    borderRadius: "45px",
+                    marginRight: "20px",
+                    marginTop : "70px",
+                    boxShadow: "6px 8px rgb(12, 9, 1)"
+                }
 
+        
+        }>
+            Name
+            <Form.Control type="text" value={user.name} placeholder="enter your name" onChange={(e)=>setUser({...user,name:e.target.value})} required ></Form.Control> 
+            Email
+            <Form.Control type="text" value={user.email} placeholder="enter your email" onChange={(e)=>setUser({...user,email:e.target.value})} required ></Form.Control> 
+            Password
+            <Form.Control type="text" value={user.password} placeholder="enter your password" onChange={(e)=>setUser({...user,password:e.target.value})}required ></Form.Control> 
+            <Button style={
+                {
+                    textAlign:"center",
+                    marginLeft:"140px",
+                    marginTop:"20px"
+                }
+            } variant="dark" onClick={submit}>Register</Button>
 
+            </Form>
 
-        <Button variant="dark" onClick={submit}>Register</Button>
-
+        </Container>
     </div>
+
 }
 export default Register
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import registerSliceReducer from './slices/register'
-
+import careergoalSlice from './slices/careergoal'
 
 
 export default configureStore({
   reducer: {
     register : registerSliceReducer,
+    careerGoal : careergoalSlice,
  
   },
 })
