907a2f5fbb60311791be8da67f0244886f1fc164
thevayani <ithevayani1601@gmail.com>
2025-03-18 16:44:59 +0530

    login completed

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 import Login from './pages/Login.jsx'
 import Register from './pages/Register.jsx' 
 import store from './redux/store.js'
