8faf1b51b16545b009aa38db97ce44fd26a56e50
thevayani <ithevayani1601@gmail.com>
2025-03-21 11:23:46 +0530

    new goal

--- src/pages/CareerGoals.jsx
+++ src/pages/CareerGoals.jsx
@@ -1,87 +1,208 @@
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button,Form } from 'react-bootstrap';
-import{useState} from "react";
-import{useNavigate} from "react-router-dom";
+import { Button, Form } from 'react-bootstrap';
+import { useState, useEffect } from "react";
+import axios from 'axios';
+import image from '../assets/goal.jpg'
+import { IoAddOutline } from "react-icons/io5";
+import { AiTwotoneDelete } from "react-icons/ai";
 
-import { setCareerUsers } from '../redux/slices/careergoal';
-import { useSelector,useDispatch } from 'react-redux';
+function CareerGoals() {
+const [goalsQues, setgoalsQues] = useState([
+        {
+            question: "what is your preferred work location?",
+            answer: ""
+        },
+        {
+            question: "what relevant certification do you hold?",
+            answer: ""
+        },
+        {
+            question: "Get a job within?",
+            answer: ""
+        },
+        {
+            question: "What is your salary range expectation?",
+            answer: ""
+        }
+    ])
 
+    const [skillDetails, setSkillDetails] = useState([])
+    const [goalsDetails, setGoalDetails] = useState({
+        goal: "",
+        skill: [],
+        questions: [],
 
-
-function CareerGoals(){
-
-    const careerGlobalState=useSelector((state)=>state.careerGoal.careerGoalUsers)
-    const dispatch=useDispatch()
-
-
-    const[skillDetails,setSkillDetails]=useState([])
-    const[goalsDetails,setGoalDetails]=useState({
-        goal:"",
-        skill:[],
-        what_industries_are_you_most_interested_in:"",
-        what_relevant_certification_do_you_hold:"",
-        period_of_time_to_get_the_job:""
     })
 
+    useEffect(() => {
+        getApi()
+    }, [])
 
 
-    const addskill=()=>{
 
-        if(skillDetails == ""){
+
+    const addskill = () => {
+
+        if (skillDetails == "") {
             alert("Please enter the value")
 
         }
-        else{
-        setSkillDetails("")
-        let value=[skillDetails]
-        console.log(value)
-        let skills=[...goalsDetails.skill,...value]
-        setGoalDetails({...goalsDetails,skill:skills})
+        else {
+            setSkillDetails("")
+            let value = [skillDetails]
+            console.log(value)
+            let skills = [...goalsDetails.skill, ...value]
+            setGoalDetails({ ...goalsDetails, skill: skills })
         }
- 
+
 
     }
 
-        const submit=()=>{
-            if(goalsDetails.goal && goalsDetails.skill && goalsDetails.what_industries_are_you_most_interested_in &&
-                goalsDetails.what_relevant_certification_do_you_hold && goalsDetails.period_of_time_to_get_the_job)
+    const deleteBtn = (v) => {
+        alert("Do you want to delete?")
+        let del = goalsDetails.skill.filter((items) => items != v)
+        setGoalDetails({ ...goalsDetails, skill: del })
+    }
 
-            {
 
-             let   data = [...careerGlobalState,goalsDetails]
-                        dispatch(setCareerUsers(data));
-                        console.log(data)
-                        alert("ok")
-                        setGoalDetails({goal:"",skill:"",what_industries_are_you_most_interested_in:"",what_relevant_certification_do_you_hold:"",
-                            period_of_time_to_get_the_job:""
-                        })
+    const handleQuestion = (index, value) => {
+        const updatedQuestions = [...goalsQues];
+
+        updatedQuestions[index].answer = value;
+        setGoalDetails({ ...goalsDetails, questions: updatedQuestions });
+    };
+
+
+
+
+
+    const submit = () => {
+
+
+        const formData = new FormData();
+        formData.append("user_id", 4);
+        formData.append("data", JSON.stringify(goalsDetails));
+
+        axios.post('https://agaram.academy/api/b4/action.php?request=ai_carrier_update_user_goals', formData).then((res) => {
+            console.log(res)
+        })
+
+
+
+        if (goalsDetails.goal && goalsDetails.skill && goalsQues[0].answer
+            && goalsQues[1].answer && goalsQues[2].answer && goalsQues[3].answer) {
+
+            console.log(goalsDetails)
+            setGoalDetails({ goal: "" })
+
+
+
+            setgoalsQues([
+                {
+                    question: "what is your preferred work location?",
+                    answer: ""
+                },
+                {
+                    question: "what relevant certification do you hold?",
+                    answer: ""
+                },
+                {
+                    question: "Get a job within?",
+                    answer: ""
+                },
+                {
+                    question: "What is your salary range expectation?",
+                    answer: ""
+                }
+            ])
+            alert("submitted")
         }
-        else{
+        else {
             alert("Please fillup")
         }
     }
 
+    const getApi = () => {
+        axios.get('https://agaram.academy/api/b4/action.php?request=ai_carrier_get_user_goals&user_id=4')
+            .then((res) => {
+                let getData = res.data.data.data
+                setGoalDetails(JSON.parse(getData))
+                console.log(getData)
+
+            })
+    }
+
+    return <div style={
+        {
+            backgroundImage: `url(${image})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            height: "1000px",
+            background : "20"
+        }
+    }>
+        <h1 style={{ textAlign: "center", color: "black" }}><i>Career Goal</i></h1>
+
+        <div style={{
+            backgroundColor: "rgb(34, 160, 195)",
+            width: "500px",
+            marginLeft: "34%",
+            borderRadius: "15px",
+            marginRight: "50%",
+            marginTop: "50px",
+            boxShadow: "0 0 10px",
+            padding: "30px"
+
+        }}>
+
+            <h5 style={{ textAlign: "left", color: "black", marginLeft: "25px" }}>Goal:</h5>
+            <Form.Control type="text" value={goalsDetails.goal} placeholder="Enter your goal" onChange={(e) => setGoalDetails({ ...goalsDetails, goal: e.target.value.trimStart() })} required  ></Form.Control>
+
+            <h5 style={{ textAlign: "left", color: "black", marginLeft: "25px" }}>skill:</h5>
 
 
-    return <div>
-        <h1>Career Goal</h1>
-        Enter your Goal
-        <Form.Control type="text" value={goalsDetails.goal}onChange={(e)=>setGoalDetails({...goalsDetails,goal:e.target.value})} required  ></Form.Control> 
-         skill 
-         <div>
-        <Form.Control type="text" value={skillDetails} onChange={(e)=>setSkillDetails(e.target.value)} required ></Form.Control>
-        <Button variant="info"onClick={addskill}>Add</Button>
+            <div>
+                <div className="d-flex align-items-center">
+                    <Form.Control
+                        type="text"
+                        value={skillDetails}
+                        onChange={(e) => setSkillDetails(e.target.value.trimStart())}
+                        required
+                        className="me-2"  
+                    />
+                    <Button variant="warning" onClick={addskill}><IoAddOutline /></Button>
+                </div>
+
+                <ul>
+                    <div>
+                    {goalsDetails.skill?.map((v) =>
+                        <li>{v} <Button variant="danger" onClick={() => deleteBtn(v)}><AiTwotoneDelete /></Button>
+
+                        </li>
+                    )}
+                    </div>
+                </ul>
+
+
+            </div>
+
+            {() => console.log(goalsDetails.questions)}
+            {goalsDetails?.questions?.length ? goalsDetails.questions.map((q, index) => (
+                <div>
+                   <b>{q.question}</b> 
+                    <Form.Control type="text" value={q.answer} onChange={(e) => handleQuestion(index, e.target.value.trimStart())} required />
+                </div>
+            )) : goalsQues.map((q, index) => (
+                <div>
+                    {q.question}
+                    <Form.Control type="text" style={{ textAlign: "left", color: "black", marginLeft: "25px" }} value={q.answer} onChange={(e) => handleQuestion(index, e.target.value.trimStart())} required />
+                </div>
+            ))}
+        <div style={{ textAlign: "center", color: "black", paddingTop:"20px"}} >
+            <Button variant="success"  onClick={submit}>Submit</Button>
+            </div>
         </div>
-
-        what industries are you most interested in?
-        <Form.Control type="text" value={goalsDetails.what_industries_are_you_most_interested_in} onChange={(e)=>setGoalDetails({...goalsDetails,what_industries_are_you_most_interested_in:e.target.value})} required ></Form.Control> 
-        what relevant certification do you hold?
-        <Form.Control type="text" value={goalsDetails.what_relevant_certification_do_you_hold} onChange={(e)=>setGoalDetails({...goalsDetails,what_relevant_certification_do_you_hold:e.target.value})} required></Form.Control> 
-        period of time to get the job?
-        <Form.Control type="text" value={goalsDetails.period_of_time_to_get_the_job} onChange={(e)=>setGoalDetails({...goalsDetails,period_of_time_to_get_the_job:e.target.value})} required></Form.Control> 
-        <Button variant="primary" onClick={submit}>Submit</Button>
-
     </div>
+
 }
- export default CareerGoals
+export default CareerGoals
